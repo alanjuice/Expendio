2fbc0f7ec76a91dbffcc617a363f66da4f81b1f7
Alan <93069058+alanjuice@users.noreply.github.com>
2024-08-15 08:47:22 +0530

    update models

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -3,8 +3,10 @@
     public class Expense
     {
         public int Id { get; set; }
-        public string? Category { get; set; }
+        public string Category { get; set; } = string.Empty;
         public int Amount { get; set; }
+
+        public DateOnly Date { get; set; } = new DateOnly();
         public User? User { get; set; }
         public int UserId { get; set; }
     }
--- Models/Income.cs
+++ Models/Income.cs
@@ -5,9 +5,11 @@ namespace Expendio.Models
     public class Income
     {
         public int Id { get; set; }
-        public string? Source { get; set; }
+        public string Source { get; set; } = string.Empty;
         public int Amount { get; set; }
+        public DateOnly Date { get; set; } = new DateOnly();
         public User? User { get; set; }
         public int UserId { get; set; }
+     
     }
 }
