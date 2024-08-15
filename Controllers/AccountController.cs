aa78687d6275e846f4e597e859df1c5697643075
Alan <93069058+alanjuice@users.noreply.github.com>
2024-08-15 08:44:56 +0530

    add basic cookie auth

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expendio.Data;
+using Expendio.DTO;
+using Expendio.Mappers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Expendio.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly ExpendioDbContext _context;
+        public AccountController(ExpendioDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -17,6 +29,58 @@ namespace Expendio.Controllers
         public IActionResult SignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUpUser(SignupUserDto signupUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            var user = signupUser.ToUser();
+            try
+            {
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception)
+            {
+                return NoContent();
+            }            
+        }
+
+
+        [HttpPost]
+        public  async Task<IActionResult> LoginUser(LoginUserDto loginUserDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u=>u.Email==loginUserDto.Email);
+            if (user == null)
+            {
+                return Unauthorized("User doesn't exist");
+            }
+            if (loginUserDto.Password != user.Password)
+            {
+                return Unauthorized("Incorrect Password");
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim("ID", user.Id.ToString()),
+                new Claim(ClaimTypes.Email,user.Email),             
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+
+            return RedirectToAction("Index", "User");
         }
     }
 }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,44 +3,100 @@ using Expendio.DTO;
 using Expendio.Mappers;
 using Expendio.Models;
 using Expendio.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Expendio.Controllers
 {
     public class UserController : Controller
     {
         private readonly ExpendioDbContext _context;
-        public UserController(ExpendioDbContext context)
+        private readonly IHttpContextAccessor _Accessor;
+        public UserController(ExpendioDbContext context, IHttpContextAccessor accessor)
         {
             _context = context;
+            _Accessor = accessor;
         }
+
+        [Authorize]
         public IActionResult Index()
         {
+            var idClaim = User.Claims.FirstOrDefault(c => c.Type == "ID")?.Value;
+            var Id = Convert.ToInt32(idClaim);
+            
             return View();
         }
 
+        [Authorize]
         public IActionResult Analytics()
         {
             return View();
         }
 
+        [Authorize]
         public IActionResult Add()
         {
             return View();
         }
 
-        public async Task<IActionResult> AddIncome(IncomeDto incomeDto, int userId)
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddIncome(int id, AddIncomeDto incomeDto)
         {
-            var income = incomeDto.ToIncome(userId);
-            await _context.Incomes.AddAsync(income);
-            return NoContent();
+            var idClaim = User.Claims.FirstOrDefault(c => c.Type == "ID")?.Value;
+            var Id = Convert.ToInt32(idClaim);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var incomeEntity = incomeDto.ToIncome(Id);
+                    await _context.Incomes.AddAsync(incomeEntity);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index", "User");
+                }
+                catch (Exception ex)
+                {
+                    return Content(ex.ToString());
+                }
+            }
+            return View(incomeDto);
         }
 
+        [Authorize]
+        public async Task<IActionResult> AddExpense(AddExpenseDto expenseDto)
+        {
+            var idClaim = User.Claims.FirstOrDefault(c => c.Type == "ID")?.Value;
+            var Id = Convert.ToInt32(idClaim);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var expenseEntity = expenseDto.ToExpense(Id);
+                    await _context.Expenses.AddAsync(expenseEntity);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index", "User");
+                }
+                catch (Exception ex)
+                {
+                    return Content(ex.ToString());
+                }
+            }
+            return View(expenseDto);
+        }
+
+        [Authorize]
         public IActionResult History() 
         {
-            var expenses = _context.Expenses.ToList<Expense>();
-            var incomes = _context.Incomes.ToList<Income>();
-            Console.WriteLine("Ss");
+            var idClaim = User.Claims.FirstOrDefault(c => c.Type == "ID")?.Value;
+            var Id = Convert.ToInt32(idClaim);
+
+
+            var expenses = _context.Expenses.Where(expense => expense.Id == Id).ToList<Expense>();
+            var incomes = _context.Incomes.Where(income => income.Id == Id).ToList<Income>();
 
             var history = new HistoryViewModel { Expenses = expenses, Incomes = incomes };
             return View(history);
